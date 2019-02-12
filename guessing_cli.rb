3a591aa4efd3fca5618ff877281cd9365b8d5a5b
abstract-animation-9914 <vm930@hotmail.com>
2019-02-12 22:50:30 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,41 +1,41 @@
-def run_guessing_game
-  input = ""
-  while input 
-      puts "Guess a number between 1 and 6."
-      input = gets.downcase.chomp.to_i
-      random_numer = rand(1..6).to_i
-          if random_numer == input
-            puts "You guessed the correct number!"
-          else
-            puts "he computer guessed #{random_numer}."
-          end 
-  break if input == "exit"
-  #puts "Goodbye"
-  end 
-end 
-
-
-
-
-
-
-
-
 # def run_guessing_game
 #   input = ""
 #   while input 
-#     puts "Guess a number between 1 and 6."
-#     input = gets.downcase.chomp
-#     random_numer = rand(1..6).to_s
-#     case input.chomp
-#     when random_numer
-#       puts "You guessed the correct number!"
-#     when "exit"
-#       puts "Goodbye!"
-#       break
-#     else 
-#       puts "The computer guessed #{random_numer}."
-#     end 
-#   end
+#       puts "Guess a number between 1 and 6."
+#       input = gets.downcase.chomp.to_i
+#       random_numer = rand(1..6).to_i
+#           if random_numer == input
+#             puts "You guessed the correct number!"
+#           else
+#             puts "he computer guessed #{random_numer}."
+#           end 
+#   break if input == "exit"
+#   #puts "Goodbye"
+#   end 
 # end 
+
+
+
+
+
+
+
+
+def run_guessing_game
+  input = ""
+  while input 
+    puts "Guess a number between 1 and 6."
+    input = gets.downcase.chomp
+    random_numer = rand(1..6).to_s
+    case input.chomp
+    when random_numer
+      puts "You guessed the correct number!"
+    when "exit"
+      puts "Goodbye!"
+      break
+    else 
+      puts "The computer guessed #{random_numer}."
+    end 
+  end
+end 
 
